797e1920c76bfa666544f2d67f06647d77f579e7
Lokeshwaran R <waranlokesh688@gmail.com>
2025-09-27 09:56:48 +0530

    fix: Loader in main page

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@
     import { Navigate, useLocation } from 'react-router-dom';
     import { useAuth } from '../hooks/useAuth.js';
     import { ROUTES } from '../utils/constants.js';
+import Loader from './ui/Loader.jsx';
 
     const ProtectedRoute = ({ children }) => {
         const { isAuthenticated, loading } = useAuth();
@@ -10,7 +11,7 @@
         if (loading) {
             return (
                 <div className="flex items-center justify-center min-h-screen">
-                    <div className="text-lg">Loading...</div>
+                    <div className="flex items-center justify-center"><Loader/></div>
                 </div>
             );
         }
